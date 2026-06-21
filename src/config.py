866a7e669dfa0cf79e3fba98fc09c1abbc90a926
SinghSaurabh2003 <127893926+SinghSaurabh2003@users.noreply.github.com>
2026-06-21 10:04:55 +0530

    config.py

--- src/config.py
+++ src/config.py
@@ -1,25 +1,26 @@
-# Dataset
-DATASET_PATH = "data/raw/Training_half"
+# Dataset 
+DATASET_PATH = "/content/data/Training_half" 
 
-# Image
-IMAGE_SIZE = 224
+# Image 
+IMAGE_SIZE = 224 
 
-# DataLoader
-BATCH_SIZE = 32
-VAL_SPLIT = 0.2
-NUM_WORKERS = 0
+# DataLoader 
+BATCH_SIZE = 32 
+VAL_SPLIT = 0.2 
+NUM_WORKERS = 2 
 
-# Training
-EPOCHS = 10
-LEARNING_RATE = 1e-4
-WEIGHT_DECAY = 1e-4
+# Training 
+EPOCHS = 10 
+LEARNING_RATE = 1e-4 
+WEIGHT_DECAY = 1e-4 
 
-# Model
-NUM_CLASSES = 9
+# Model 
+NUM_CLASSES = 9 
 
-# Seed
-SEED = 42
+# Seed 
+SEED = 42 
 
-# Save
-CHECKPOINT_DIR = "checkpoints"
-BEST_MODEL_NAME = "best_model.pth"
+# Save 
+CHECKPOINT_DIR = "/content/drive/MyDrive/Document-Type-Classifier/checkpoints" 
+BEST_MODEL_NAME = "best_model.pth" 
+LAST_MODEL_NAME = "last_model.pth"
